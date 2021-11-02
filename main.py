234d513541b4d0e7080a5c8884f635fe4a40a28d
Gabriel Teles <93494655+DevM4yCry@users.noreply.github.com>
2021-11-02 11:53:38 -0300

    2:17

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def dia_para_segundos(num_dias):
 def validacao():
     try:
 
-        total_dias = int(user_input)
+        total_dias = int(num_dias_element)
         if total_dias > 0:
             calculo = dia_para_segundos(total_dias)
             print(calculo)
@@ -26,13 +26,18 @@ def validacao():
     except ValueError:
         print("Não tente burlar a entrada.")
 
+
 # Restarting the application to keep running
 # Looping could be done in Python with while or for
 # Input
 print("Quando quiser sair da aplicação digite: sair ")
 user_input = 0
 while user_input != "sair":
-    user_input = input("Digite o número de dias e te darei o total de segundos:")
-    validacao()
+    user_input = input("Digite o número de dias e te darei o total de segundos (exemplo: 1, 2, 3):")
+    # Every user input will be save in a list
+    # Split function will give us a list of the input values
+    print(type(user_input.split(",")))
+    print(user_input.split(","))
+    for num_dias_element in user_input.split(","):
 
-
+        validacao()
