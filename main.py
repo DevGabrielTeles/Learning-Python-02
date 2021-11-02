fc41332964e958999cfa487e0fc49937caaa26e3
Gabriel Teles <93494655+DevM4yCry@users.noreply.github.com>
2021-11-02 11:15:14 -0300

    1:47

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ minuto_para_segundo = 60
 
 # Function with if statement
 def dia_para_segundos(num_dias):
-    if num_dias == 1:
-        return f"{num_dias} dia são {num_dias * dia_para_hora * hora_para_minuto * minuto_para_segundo} segundos"
-    elif num_dias > 1:
-        return f"{num_dias} dias são {num_dias * dia_para_hora * hora_para_minuto * minuto_para_segundo} segundos"
-    elif num_dias == 0:
-        return "Você inseriu um valor inválido, insira um valor positivo diferente de 0."
-    else:
-        return "Você inseriu um valor negativo."
+    return f"{num_dias} dia(s) são {num_dias * dia_para_hora * hora_para_minuto * minuto_para_segundo} segundos"
 
 
 # Validando dado string
 def validacao():
-    if total_dias.isdigit():
-        funcao_calculada = dia_para_segundos(int(total_dias))
-        # Calling function
-        print(funcao_calculada)
-    else:
-        print("Não atrasa meu lado!")
+    try:
+
+        total_dias = int(user_input)
+        if total_dias > 0:
+            calculo = dia_para_segundos(total_dias)
+            print(calculo)
+        elif total_dias == 0:
+            print("Você inseriu um valor inválido, insira um valor positivo diferente de 0.")
+        elif total_dias < 0:
+            print("Você inseriu um valor inválido, insira um valor positivo diferente de 0.")
+
+
+    except ValueError:
+        print("Não tente burlar a entrada.")
 
 
 # Input
-total_dias = input("Digite o número de dias e te darei o total de segundos:")
+user_input = input("Digite o número de dias e te darei o total de segundos:")
 validacao()
